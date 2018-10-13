797e23ccf12ce991410c3194200aceb9253872fe
xVanieeeeeeeet <42171095+xVanieeeeeeeet@users.noreply.github.com>
2018-10-13 17:59:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -120,67 +120,52 @@ client.on('ready', () => {
 
  });
 
-client.on('message', message => {
-   if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + 'bc')) {
-if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-let BcList = new Discord.RichEmbed()
-.setThumbnail(message.author.avatarURL)
-.setAuthor(`محتوى الرساله ${args}`)
-.setDescription(`برودكاست بـ امبد 📝\nبرودكاست بدون امبد✏ \nلديك دقيقه للأختيار قبل الغاء البرودكاست`)
-if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(BcList).then(msg => {
-msg.react('📝')
-.then(() => msg.react('✏'))
-.then(() =>msg.react('📝'))
- 
-let EmbedBcFilter = (reaction, user) => reaction.emoji.name === '📝' && user.id === message.author.id;
-let NormalBcFilter = (reaction, user) => reaction.emoji.name === '✏' && user.id === message.author.id;
- 
-let EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
-let NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
- 
-EmbedBc.on("collect", r => {
-  message.guild.members.forEach(m => {
-    let far = new Discord.RichEmbed()
-      .setTitle('New Message.')
-      .addField('Server:', `${message.guild.name}`)
-      .addField('Author:', `${message.author.username}`)
-      .addField('Message:', `${args}\n${m}`)
-      .setColor("RANDOM")
-      .setFooter(`${client.user.username}`)
-      .setThumbnail(`${m.user.avatarURL}`);
-        m.sendEmbed(far)
+ client.on('message', message => {
+    if(!message.channel.guild) return;
+ if(message.content.startsWith(prefix + 'bc')) {
+ if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+ if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+ let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+ let BcList = new Discord.RichEmbed()
+ .setThumbnail(message.author.avatarURL)
+ .setAuthor(`محتوى الرساله ${args}`)
+ .setDescription(`برودكاست بـ امبد 📝\nبرودكاست بدون امبد✏ \nلديك دقيقه للأختيار قبل الغاء البرودكاست`)
+ if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(BcList).then(msg => {
+ msg.react('📝')
+ .then(() => msg.react('✏'))
+ .then(() =>msg.react('📝'))
+  
+ let EmbedBcFilter = (reaction, user) => reaction.emoji.name === '📝' && user.id === message.author.id;
+ let NormalBcFilter = (reaction, user) => reaction.emoji.name === '✏' && user.id === message.author.id;
+  
+ let EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
+ let NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
+  
+ EmbedBc.on("collect", r => {
+ message.guild.members.forEach(m => {
+ var bc = new
+ Discord.RichEmbed()
+ .setColor('RANDOM')
+   .setTitle("New Message")
+ .addField('Server : ', `${message.guild.name}`)
+ .addField('Author : ',`${message.author.username}`)
+.addField('Message :', `${args}\n${m}`)
+ .setThumbnail(message.author.avatarURL)
+ m.send({ embed: bc })
+ msg.delete();
+ message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
+ })
+ })
+ NormalBc.on("collect", r => {
+ message.guild.members.forEach(m => {
+ m.send(`${args}\n${m}`);
+ msg.delete();
+ message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
 
-    let embed = new Discord.RichEmbed()
-          .setColor("RANDOM")
-      .setAuthor(`Sent to ${message.guild.memberCount} members!`);
-      message.channel.sendEmbed(embed) .then(x => {
-        x.delete(3000);
-      });
-    if(message.attachments.first()) {
-      m.sendFile(message.attachments.first().url).catch();
-    }
-    msg.delete();
-})
-})
-NormalBc.on("collect", r => {
-  message.guild.members.forEach(m => {
-    m.send(`${args}\n${m}`)
-          .setColor("RANDOM")
-      .setAuthor(`Sent to ${message.guild.memberCount} members!`);
-      message.channel.sendEmbed(embed) .then(x => {
-        x.delete(3000);
-      });
-    if(message.attachments.first()) {
-      m.sendFile(message.attachments.first().url).catch();
-    }
-msg.delete();
-})
-})
-})
-}
-});
+ })
+ })
+ })
+ }
+ });
 
 client.login(process.env.BOT_TOKEN);
